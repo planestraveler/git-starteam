627ebe225d22a6618d62a32760f2fb5e9410c9ea
Steve Tousignant <s.tousignant@gmail.com>
2011-06-11 23:43:11 -0400

    Made some test command to see how it works

--- syncronizer/src/org/sync/MainEntry.java
+++ syncronizer/src/org/sync/MainEntry.java
@@ -1,10 +1,21 @@
 package org.sync;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.IllegalFormatException;
+import java.util.Random;
 
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.InitCommand;
+import org.eclipse.jgit.api.errors.ConcurrentRefUpdateException;
+import org.eclipse.jgit.api.errors.JGitInternalException;
+import org.eclipse.jgit.api.errors.NoFilepatternException;
+import org.eclipse.jgit.api.errors.NoHeadException;
+import org.eclipse.jgit.api.errors.NoMessageException;
+import org.eclipse.jgit.api.errors.WrongRepositoryStateException;
 
 public class MainEntry {
 
@@ -14,18 +25,36 @@ public class MainEntry {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		File testFolder = new File("/tmp/gitTest");
-		if(testFolder.exists() && testFolder.isDirectory()) {
-			try {
-				Git aGitDB = Git.open(testFolder);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		} else {
+		File testFolder = new File("/tmp/gitTestB");
+		if(!testFolder.exists()) {
 			InitCommand init = new InitCommand();
-			init.setBare(true);
 			init.setDirectory(testFolder);
 			init.call();
+		}
+		try {
+			Git aGitDB = Git.open(testFolder);
+			aGitDB.add().addFilepattern("a/b").call();
+			aGitDB.commit().setAuthor("Bob the builder", "build@fast.com").setMessage("This is a short test").call();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (NoHeadException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoMessageException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ConcurrentRefUpdateException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JGitInternalException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (WrongRepositoryStateException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoFilepatternException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 
