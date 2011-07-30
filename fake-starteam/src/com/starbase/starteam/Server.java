62300b791d69971d679e6b880588f77d71c7f5dc
Steve Tousignant <s.tousignant@gmail.com>
2011-07-30 15:09:42 -0400

    Fix license on all files and try to prevent legal problems

--- fake-starteam/src/com/starbase/starteam/Server.java
+++ fake-starteam/src/com/starbase/starteam/Server.java
@@ -1,3 +1,19 @@
+/*****************************************************************************
+ * All public interface based on Starteam API are a property of Borland, 
+ * those interface are reproduced here only for testing purpose. You should
+ * never use those interface to create a competitive product to the Starteam
+ * Server. 
+ * 
+ * The implementation is given AS-IS and should not be considered a reference 
+ * to the API. The behavior on a lots of method and class will not be the
+ * same as the real API. The reproduction only seek to mimic some basic 
+ * operation. You will not found anything here that can be deduced by using
+ * the real API.
+ * 
+ * Fake-Starteam is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *****************************************************************************/
 package com.starbase.starteam;
 
 import org.ossnoize.fakestarteam.InternalPropertiesProvider;
--- fake-starteam/src/org/ossnoize/fakestarteam/InternalPropertiesProvider.java
+++ fake-starteam/src/org/ossnoize/fakestarteam/InternalPropertiesProvider.java
@@ -4,6 +4,22 @@ import java.io.File;
 import java.io.IOException;
 import com.starbase.starteam.Server;
 
+/*****************************************************************************
+ * All public interface based on Starteam API are a property of Borland, 
+ * those interface are reproduced here only for testing purpose. You should
+ * never use those interface to create a competitive product to the Starteam
+ * Server. 
+ * 
+ * The implementation is given AS-IS and should not be considered a reference 
+ * to the API. The behavior on a lots of method and class will not be the
+ * same as the real API. The reproduction only seek to mimic some basic 
+ * operation. You will not found anything here that can be deduced by using
+ * the real API.
+ * 
+ * Fake-Starteam is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *****************************************************************************/
 public class InternalPropertiesProvider {
 
 	private static InternalPropertiesProvider Reference = null;
