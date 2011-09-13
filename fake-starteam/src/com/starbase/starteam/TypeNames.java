421ac724285a688d263b778f7411e0b309190d7a
Steve Tousignant <s.tousignant@gmail.com>
2011-09-12 21:28:34 -0400

    Added the list of TypeNames
    Could be useful.

---
+++ fake-starteam/src/com/starbase/starteam/TypeNames.java
@@ -0,0 +1,41 @@
+/*****************************************************************************
+ * All public interface based on Starteam API are a property of Borland, 
+ * those interface are reproduced here only for testing purpose. You should
+ * never use those interface to create a competitive product to the Starteam
+ * Server. 
+ * 
+ * The implementation is given AS-IS and should not be considered a reference 
+ * to the API. The behavior on a lots of method and class will not be the
+ * same as the real API. The reproduction only seek to mimic some basic 
+ * operation. You will not found anything here that can be deduced by using
+ * the real API.
+ * 
+ * Fake-Starteam is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *****************************************************************************/
+package com.starbase.starteam;
+
+public class TypeNames {
+	public final String	AUDIT = "Audit";
+	public final String CHANGEREQUEST = "ChangeRequest";
+	public final String FILE = "File";
+	public final String FOLDER = "Folder";
+	public final String ITEM = "Item";
+	public final String ITEM_REFERENCE = "ItemReference";
+	public final String ITEM_REVISION = "ItemRevision";
+	public final String LABEL = "Label";
+	public final String LINK = "Link";
+	public final String PROJECT = "Project";
+	public final String PROMOTIONMODEL = "PromotionModel";
+	public final String PROMOTIONSTATE = "PromotionState";
+	public final String REQUIREMENT = "Requirement";
+	public final String TASK = "Task";
+	public final String TASKDEPENDENCY = "TaskDependency";
+	public final String TOPIC = "Topic";
+	public final String TYPE = "Type";
+	public final String USER = "User";
+	public final String USERACCOUNT = "User";
+	public final String VIEW = "View";
+	public final String WORKRECORD = "WorkRecord";
+}
