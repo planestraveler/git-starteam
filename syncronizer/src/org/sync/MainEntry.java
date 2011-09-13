3238072bb452347a16c6ab47db9995228cb1badb
Steve Tousignant <s.tousignant@gmail.com>
2011-09-12 20:36:09 -0400

    Wont be using egit for now.
    Effort will be put for the fast-import part of the software.

--- syncronizer/src/org/sync/MainEntry.java
+++ syncronizer/src/org/sync/MainEntry.java
@@ -16,57 +16,13 @@
 ******************************************************************************/
 package org.sync;
 
-import java.io.File;
-import java.io.IOException;
-
-import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.api.InitCommand;
-import org.eclipse.jgit.api.errors.ConcurrentRefUpdateException;
-import org.eclipse.jgit.api.errors.JGitInternalException;
-import org.eclipse.jgit.api.errors.NoFilepatternException;
-import org.eclipse.jgit.api.errors.NoHeadException;
-import org.eclipse.jgit.api.errors.NoMessageException;
-import org.eclipse.jgit.api.errors.WrongRepositoryStateException;
-
 public class MainEntry {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-		File testFolder = new File("/tmp/gitTestB");
-		if(!testFolder.exists()) {
-			InitCommand init = new InitCommand();
-			init.setDirectory(testFolder);
-			init.call();
-		}
-		try {
-			Git aGitDB = Git.open(testFolder);
-			aGitDB.add().addFilepattern("a/b").call();
-			aGitDB.commit().setAuthor("Bob the builder", "build@fast.com").setMessage("This is a short test").call();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (NoHeadException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (NoMessageException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ConcurrentRefUpdateException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (JGitInternalException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (WrongRepositoryStateException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (NoFilepatternException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 	}
 
 }
