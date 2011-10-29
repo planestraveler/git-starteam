7a70996c6e200d0a033766a950e7f4f8abb50f5a
Steve Tousignant <s.tousignant@gmail.com>
2011-10-29 19:31:37 -0400

    Added a new type of exception.
    From the error class, notify if the operation done is valid.

---
+++ fake-starteam/src/org/ossnoize/fakestarteam/exception/InvalidOperationException.java
@@ -0,0 +1,13 @@
+package org.ossnoize.fakestarteam.exception;
+
+public class InvalidOperationException extends Error {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5270583125572733054L;
+
+	public InvalidOperationException(String desc) {
+		super(desc);
+	}
+}
