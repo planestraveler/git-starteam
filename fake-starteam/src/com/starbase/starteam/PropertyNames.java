068d476fb68fea36a938aec48503cb77770374b7
Steve Tousignant <s.tousignant@gmail.com>
2011-12-02 16:44:38 -0500

    Added File_description property.

--- fake-starteam/src/com/starbase/starteam/PropertyNames.java
+++ fake-starteam/src/com/starbase/starteam/PropertyNames.java
@@ -20,4 +20,5 @@ public class PropertyNames {
 	public final java.lang.String COMMENT = "Comment";
 	public final java.lang.String FOLDER_NAME = "Name";
 	public final java.lang.String OBJECT_ID = "Object ID";
+	public final java.lang.String FILE_DESCRIPTION = "Description";
 }
