fa3525613a2e2bf358f2a8742794761a797b26a9
Steve Tousignant <s.tousignant@gmail.com>
2011-12-04 16:18:34 -0500

    Make use of File with the Data object.
    This may be more useful since the information may be massive and we may not have that much memory in the converting computer.

--- syncronizer/src/org/ossnoize/git/fastimport/Data.java
+++ syncronizer/src/org/ossnoize/git/fastimport/Data.java
@@ -17,19 +17,30 @@
 package org.ossnoize.git.fastimport;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 
 public class Data implements FastImportObject {
 	private static final String DATA = "data";
 	private ByteArrayOutputStream Container;
+	private File file;
 	
 	public Data() {
 		Container = new ByteArrayOutputStream();
 	}
 	
-	public OutputStream getOutputStream() {
-		return Container;
+	public Data(File f) {
+		file = f;
+	}
+	
+	public OutputStream getOutputStream() throws IOException {
+		if(null != Container) {
+			return Container;
+		}
+		return null;
 	}
 	
 	public void writeData(byte[] array) throws IOException {
@@ -39,9 +50,25 @@ public class Data implements FastImportObject {
 	@Override
 	public void writeTo(OutputStream out) throws IOException {
 		StringBuilder builder = new StringBuilder();
-		builder.append(DATA).append(" ").append(Container.size()).append("\n");
+		builder.append(DATA).append(" ");
+		if(null != Container) {
+			builder.append(Container.size());
+		} else {
+			builder.append(file.length());
+		}
+		builder.append("\n");
 		out.write(builder.toString().getBytes());
-		Container.writeTo(out);
+		if(null != Container) {
+			Container.writeTo(out);
+		} else {
+			FileInputStream fin = new FileInputStream(file);
+			byte[] buffer = new byte[1024*64];
+			int read = fin.read(buffer);
+			while(read >= 0) {
+				out.write(buffer, 0, read);
+				read = fin.read(buffer);
+			}
+		}
 		out.write('\n');
 	}
 
