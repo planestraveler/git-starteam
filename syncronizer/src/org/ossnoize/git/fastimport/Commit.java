085caf8ff96ef08577aa966b6a80f02eae18adfb
Steve Tousignant <s.tousignant@gmail.com>
2011-12-05 22:50:44 -0500

    Fixed a bit of the commit message

--- syncronizer/src/org/ossnoize/git/fastimport/Commit.java
+++ syncronizer/src/org/ossnoize/git/fastimport/Commit.java
@@ -2,6 +2,7 @@ package org.ossnoize.git.fastimport;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -10,24 +11,28 @@ public class Commit implements Markable {
 	private final static String COMMIT = "commit";
 	private final static String AUTHOR = "author";
 	private final static String COMMITTER = "committer";
-	private final static SimpleDateFormat DATEFORMAT = new SimpleDateFormat()
+	private final static SimpleDateFormat DATEFORMAT = new SimpleDateFormat("Z");
 
 	private Mark mark;
 	private String authorName;
 	private String authorEmail;
 	private String commiterName;
 	private String commiterEmail;
-	private String commitMessage;
 	private String reference;
+	private Data comment;
 	private Commit from;
 	private Commit merge;
 	private List<FileOperation> listOfOperation;
 	private Date commitDate;
 
-	public Commit(String name, String email, String message, String reference, java.util.Date commitDate) {
+	public Commit(String name, String email, String message, String reference, java.util.Date commitDate) throws IOException {
+		if(null == message) {
+			throw new NullPointerException("Message cannot be Null");
+		}
 		commiterName = name;
 		commiterEmail = email;
-		commitMessage = message;
+		comment = new Data();
+		comment.writeData(message.getBytes());
 		this.reference = reference;
 		this.commitDate = commitDate;
 		mark = new Mark();
@@ -60,15 +65,20 @@ public class Commit implements Markable {
 		commitMsg = new StringBuilder();
 		if(null != authorName  && null != authorEmail) {
 			commitMsg.append(AUTHOR).append(' ').append(authorName).append(' ')
-					 .append('<').append(authorEmail).append('>').append(' ');
+					 .append('<').append(authorEmail).append('>').append(' ')
+					 .append(commitDate.getTime()).append(' ').append(DATEFORMAT.format(commitDate))
+					 .append('\n');
 		}
 		commitMsg.append(COMMITTER).append(' ').append(commiterName).append(' ')
-				 .append('<').append(commiterEmail).append('>').append(' ');
+				 .append('<').append(commiterEmail).append('>').append(' ')
+				 .append(commitDate.getTime()).append(' ').append(DATEFORMAT.format(commitDate))
+				 .append('\n');
+		out.write(commitMsg.toString().getBytes());
+		comment.writeTo(out);
 	}
 
 	@Override
 	public MarkID getMarkID() {
 		return mark.getID();
 	}
-
 }
