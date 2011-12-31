122b9cf7b213c29348834c6ca85b6ca1c776e733
Steve Tousignant <s.tousignant@gmail.com>
2011-12-31 13:48:47 -0500

    Added an helper to copy the stream over.

--- fake-starteam/src/org/ossnoize/fakestarteam/FileUtility.java
+++ fake-starteam/src/org/ossnoize/fakestarteam/FileUtility.java
@@ -2,8 +2,11 @@ package org.ossnoize.fakestarteam;
 
 import java.io.Closeable;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class FileUtility {
+	private final static int BUFFER_SIZE = 64*1024;
 
 	public static void close(Closeable ... list) {
 		for(Closeable c : list) {
@@ -14,5 +17,16 @@ public class FileUtility {
 				}
 			}
 		}
+	}
+	
+	public static void copyStream(OutputStream out, InputStream in) throws IOException {
+		byte[] buffer = new byte[BUFFER_SIZE];
+		
+		int read = in.read(buffer);
+		while(read >= 0) {
+			out.write(buffer, 0, read);
+			read = in.read(buffer);
+		}
+		out.flush();
 	}
 }
