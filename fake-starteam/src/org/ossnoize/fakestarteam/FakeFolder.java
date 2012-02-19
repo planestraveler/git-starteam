4540ee01974050c3046d7c66907ac81e56300224
Steve Tousignant <s.tousignant@gmail.com>
2012-02-19 09:46:48 -0500

    Added fake folder to abstract storage of folders a little more.

---
+++ fake-starteam/src/org/ossnoize/fakestarteam/FakeFolder.java
@@ -0,0 +1,31 @@
+package org.ossnoize.fakestarteam;
+
+import java.io.File;
+import java.io.IOException;
+
+import com.starbase.starteam.Folder;
+import com.starbase.starteam.View;
+
+public class FakeFolder extends Folder {
+	
+	protected FakeFolder(View view, int objectID) {
+		this.parent = null;
+		this.view = view;
+		File serverArchive = InternalPropertiesProvider.getInstance().getFile();
+		if(!serverArchive.isDirectory()) {
+			throw new UnsupportedOperationException("The archive need to be a directory.");
+		}
+		try {
+			holdingPlace = new File(serverArchive.getCanonicalPath() + File.separator +
+					this.view.getProject().getName() + File.separator + objectID);
+			if(holdingPlace.exists() && holdingPlace.isDirectory()) {
+				validateHoldingPlace();
+				loadFolderProperties();
+			}
+			setName(view.getName());
+			update();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
