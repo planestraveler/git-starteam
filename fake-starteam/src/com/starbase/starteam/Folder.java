c36f2df8ce8e7b42aa0e59ca9f2a0bfddc05f4c8
Steve Tousignant <s.tousignant@gmail.com>
2012-05-26 17:11:07 -0400

    Fixed the shareTo logic when creating a new folder.

--- fake-starteam/src/com/starbase/starteam/Folder.java
+++ fake-starteam/src/com/starbase/starteam/Folder.java
@@ -77,7 +77,7 @@ public class Folder extends Item {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		parent.shareTo(this);
+		shareTo(parent);
 	}
 
 	protected void validateHoldingPlace() {
