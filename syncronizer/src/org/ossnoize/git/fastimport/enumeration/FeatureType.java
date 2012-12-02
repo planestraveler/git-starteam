c032604a9febc7780bd9511d73980d12ea3cf24b
Steve Tousignant <s.tousignant@gmail.com>
2012-12-02 14:15:05 -0500

    Added the feature type enumeration

---
+++ syncronizer/src/org/ossnoize/git/fastimport/enumeration/FeatureType.java
@@ -0,0 +1,40 @@
+/*****************************************************************************
+    This file is part of Git-Starteam.
+
+    Git-Starteam is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Git-Starteam is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Git-Starteam.  If not, see <http://www.gnu.org/licenses/>.
+******************************************************************************/
+package org.ossnoize.git.fastimport.enumeration;
+
+public enum FeatureType {
+	CatBlob("cat-blob", false),
+	Done("done", false);
+	
+
+	private String feature;
+	private boolean arguments;
+
+	public String getName() {
+		return feature;
+	}
+	
+	public boolean hasArguments() {
+		return arguments;
+	}
+
+	private FeatureType(String feature, boolean args) {
+		this.feature = feature;
+		this.arguments = args;
+	}
+
+}
