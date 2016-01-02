46ef15681d7f4a62905ccc869f91cd80c376b3af
Steve Tousignant <s.tousignant@gmail.com>
2016-01-02 14:42:47 -0500

    Add a first implementation of a LFS pointer.
    This shall help convert starteam repository with big files stored in
    them.

---
+++ syncronizer/src/org/ossnoize/git/fastimport/LFSFilePointer.java
@@ -0,0 +1,85 @@
+/*****************************************************************************
+    This file is part of Git-Starteam.
+
+    Git-Starteam is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Git-Starteam is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Git-Starteam.  If not, see <http://www.gnu.org/licenses/>.
+******************************************************************************/
+package org.ossnoize.git.fastimport;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+
+/**
+ *
+ * @author steve
+ */
+public class LFSFilePointer extends Data {
+  
+  private MessageDigest mDigester;
+  private File mFileToDigest;
+  private String mGitBaseFolder;
+  
+  public LFSFilePointer(String baseGitFolder, File temporaryFile) throws NoSuchAlgorithmException {
+    super();
+    mDigester = MessageDigest.getInstance("SHA-256");
+    mGitBaseFolder = baseGitFolder;
+    mFileToDigest = temporaryFile;
+  }
+  
+  @Override
+  public void writeTo(OutputStream output) throws IOException {
+    FileInputStream largeFile = new FileInputStream(mFileToDigest);
+    long sizeOfLargeFile = mFileToDigest.length();
+    
+    File lfsDir = new File(mGitBaseFolder + File.separator + "lfs" + File.separator + "objects");
+    
+    if (!lfsDir.exists()) {
+      Files.createDirectories(lfsDir.toPath());
+    }
+    File lfsLargeFile = new File(lfsDir.toString(), "starteam-lfs.tmp");
+    
+    FileOutputStream tempDestination = new FileOutputStream(lfsLargeFile);
+    byte[] buffer = new byte[4096 * 20];
+    
+    int readed = largeFile.read(buffer);
+    while (readed >= 0) {
+      mDigester.update(buffer, 0, readed);
+      tempDestination.write(buffer, 0, readed);
+      
+      readed = largeFile.read(buffer);
+    }
+    
+    byte[] sha256Digest = mDigester.digest();
+    String digest = String.format("%064x", new java.math.BigInteger(1, sha256Digest));
+    
+    File child1 = new File(lfsDir, digest.substring(0, 2));
+    File child2 = new File(child1, digest.substring(2, 4));
+    if (!child2.exists()) {
+      child2.mkdirs();
+    }
+    File finalDestination = new File(child2, digest);
+    lfsLargeFile.renameTo(finalDestination);
+
+    super.writeData(("version https://git-lfs.github.com/spec/v1\n" +
+                     "oid sha256:" + digest + "\n" +
+                     "size " + sizeOfLargeFile + "\n").getBytes("UTF-8"));
+    super.writeTo(output);
+  }
+}
---
+++ syncronizer/test/org/ossnoize/git/fastimport/test/LFSFilePointerTest.java
@@ -0,0 +1,65 @@
+/*****************************************************************************
+    This file is part of Git-Starteam.
+
+    Git-Starteam is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Git-Starteam is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Git-Starteam.  If not, see <http://www.gnu.org/licenses/>.
+******************************************************************************/
+package org.ossnoize.git.fastimport.test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.security.NoSuchAlgorithmException;
+import org.junit.After;
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+import org.ossnoize.git.fastimport.LFSFilePointer;
+
+/**
+ *
+ * @author steve
+ */
+public class LFSFilePointerTest {
+  
+  private ByteArrayOutputStream mOutput = new ByteArrayOutputStream(2048);
+  private LFSFilePointer mPointer;
+  
+  @Before
+  public void SetUp() throws NoSuchAlgorithmException, IOException  {
+    mOutput.reset();
+    Path tempDirectory = Files.createTempDirectory("LFSTest");
+    
+    mPointer = new LFSFilePointer(tempDirectory.toString(), new File("../testfiles/bigfile.bin"));
+  }
+  
+  @After
+  public void TearDown() {
+    mOutput.reset();
+  }
+  
+  @Test
+  public void testGenerateFromBigFile() throws IOException {
+    mPointer.writeTo(mOutput);
+    
+    assertEquals(
+      "data 134\n" + // Since this is a data extension we use it to calculate the actual pointer size
+      "version https://git-lfs.github.com/spec/v1\n" +
+      "oid sha256:de73c67cc17aba8cad0561ad6391d4a1146a81ed7af70efbfd42987599a04761\n" +
+      "size 104857600\n\n", // a data command put an extra line feed at the end of the command 
+      mOutput.toString("UTF-8"));
+  }
+  
+}
