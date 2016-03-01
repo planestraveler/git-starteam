b44f517774ae4ae308b6936a849958555869dad6
Steve Tousignant <s.tousignant@gmail.com>
2016-02-29 19:25:19 -0500

    Added a new commit ID to enable more test with LFS

---
+++ fake-starteam/src/org/ossnoize/fakestarteam/builder/checkin/Id13.java
@@ -0,0 +1,57 @@
+/*
+ * All public interface based on Starteam API are a property of Borland, 
+ * those interface are reproduced here only for testing purpose. You should
+ * never use those interface to create a competitive product to the Starteam
+ * Server. 
+ * 
+ * The implementation is given AS-IS and should not be considered a reference 
+ * to the API. The behavior on a lots of method and class will not be the
+ * same as the real API. The reproduction only seek to mimic some basic 
+ * operation. You will not found anything here that can be deduced by using
+ * the real API.
+ * 
+ * Fake-Starteam is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+package org.ossnoize.fakestarteam.builder.checkin;
+
+import com.starbase.starteam.File;
+import com.starbase.starteam.Folder;
+import com.starbase.starteam.View;
+import com.starbase.util.OLEDate;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Calendar;
+import org.ossnoize.fakestarteam.builder.CheckInInstruction;
+
+/**
+ * 
+ * @author steve
+ */
+public class Id13 extends CheckInInstruction {
+
+  @Override
+  public void checkin(View view) throws IOException {
+    Folder root = view.getRootFolder();
+		Folder archive = findFolderIn(root, "archive");
+    
+    
+    File fileRollerv3163 = new File(archive);
+    URL fileRollerDL = new URL("http://gentoo.mirrors.tera-byte.com/distfiles/file-roller-3.16.3.tar.xz");
+    fileRollerv3163.addFromStream(fileRollerDL.openStream(),
+      "file-roller-3.16.3.tar.xz",
+      "Source archive of file-roller 3.16.3",
+      "Source archive of file-roller 3.16.3", 0);
+    
+    view.createViewLabel("Check-in Id 13", "Check Id 13 description", new OLEDate(getTimeOfCheckIn() + 1000), true, true);
+  }
+
+  @Override
+  public long getTimeOfCheckIn() {
+    Calendar time = Calendar.getInstance();
+    time.set(2016, 1, 29, 9, 10);
+    return time.getTimeInMillis();
+  }
+  
+}
