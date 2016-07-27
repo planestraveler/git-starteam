ccdd1d3d1ac764d8d89a8dfada4f858ae01f3e67
Steve Tousignant <planestraveler@users.noreply.github.com>
2016-07-27 12:50:41 -0400

    Correct the missing quote removed during last changes

--- fake-starteam/src/com/starbase/starteam/PropertyNames.java
+++ fake-starteam/src/com/starbase/starteam/PropertyNames.java
@@ -40,7 +40,7 @@ public class PropertyNames {
 	public final java.lang.String MODIFIED_TIME = "Modified Time";
 	public final java.lang.String CREATED_TIME = "Created Time";
 	public final java.lang.String DELETED_TIME = "Deleted Time";
-	public final java.lang.String CR_CHANGE_NUMBER = "ChangeNumber;
+	public final java.lang.String CR_CHANGE_NUMBER = "ChangeNumber";
 	public final static java.lang.String ITEM_DELETED_TIME = "Item Deleted Time";
 	public final static java.lang.String ITEM_DELETED_USER_ID = "Item Deleted By";
 	protected final java.lang.String _CHILD_FOLDER = ".Child Folder ID list";
