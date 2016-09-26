060a1480228a9bba26d4cb4a117316b0a39ca832
Steve Tousignant <steve.tousignant@cae.com>
2016-09-26 17:06:49 -0400

    Initialize second to 59 as we don't know in which second the labels was built

--- syncronizer/src/org/sync/util/RevisionDateComparator.java
+++ syncronizer/src/org/sync/util/RevisionDateComparator.java
@@ -50,6 +50,7 @@ private static String buildDateFormat = "MM/dd/yy hh:mm a";
 		return Fallback.compare(arg0.getName(), arg1.getName());
 	}
 	
+	@SuppressWarnings("deprecation")
 	public static OLEDate getLabelDate(Label revisionLabel) throws ParseException{
 		String labelDescription = revisionLabel.getDescription();
 		int buildDateDescriptionIndex = labelDescription.indexOf(buildDateToken);
@@ -62,7 +63,10 @@ private static String buildDateFormat = "MM/dd/yy hh:mm a";
 
 			DateFormat dateFormat = new java.text.SimpleDateFormat(buildDateFormat);
 
-			return new OLEDate(dateFormat.parse(date.trim()));
+			java.util.Date buildDate = dateFormat.parse(date.trim());
+			buildDate.setSeconds(59); // TODO: Find a better way to initialize the
+			                          // secondes in a more proper way.
+			return new OLEDate(buildDate);
 		}
 		return revisionLabel.getRevisionTime();
 	}
